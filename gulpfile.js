7b07a9b100ef4d68c1447c2b74546a61a322b025
Philip Walton <philip@philipwalton.com>
2015-05-07 16:32:19 -0700

    Remove the babel dependency and convert the module to es5.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,13 @@
-import babelify from 'babelify';
-import browserify from 'browserify';
-import buffer from 'vinyl-buffer';
-import gulp from 'gulp';
-import rename from 'gulp-rename';
-import source from 'vinyl-source-stream';
-import uglify from 'gulp-uglify';
+var browserify = require('browserify');
+var buffer = require('vinyl-buffer');
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
 
 
 gulp.task('javascript', function() {
   return browserify('./index.js', {standalone: 'Selectable'})
-      .transform(babelify)
       .bundle()
       .pipe(source('index.js'))
       .pipe(buffer())
--- index.js
+++ index.js
@@ -1,79 +1,85 @@
-export default class Selectable {
+/**
+ * @constructor
+ */
+function Selectable(root) {
+  this.root = root;
+}
 
-  constructor(root) {
-    this.root = root;
+
+/**
+ * Gets the instances root element's text selection start and end index. The
+ * indexes are based on the text content, regardless of the HTML structure.
+ * @return {{startIndex: number, endIndex: number}}
+ */
+Selectable.prototype.get = function() {
+
+  var index = 0;
+  var startIndex;
+  var endIndex;
+  var selection = window.getSelection();
+  var treeWalker = createTextNodeTreeWalker(this.root);
+
+  while (treeWalker.nextNode()) {
+    if (treeWalker.currentNode == selection.anchorNode) {
+      startIndex = index + selection.anchorOffset;
+    }
+    if (treeWalker.currentNode == selection.focusNode) {
+      endIndex = index + selection.focusOffset;
+      break;
+    }
+    index += treeWalker.currentNode.length;
   }
 
-  /**
-   * Gets the instances root element's text selection start and end index. The
-   * indexes are based on the text content, regardless of the HTML structure.
-   * @return {{startIndex: number, endIndex: number}}
-   */
-  get() {
-
-    let index = 0;
-    let startIndex;
-    let endIndex;
-    let selection = window.getSelection();
-    let treeWalker = createTextNodeTreeWalker(this.root);
-
-    while (treeWalker.nextNode()) {
-      if (treeWalker.currentNode == selection.anchorNode) {
-        startIndex = index + selection.anchorOffset;
-      }
-      if (treeWalker.currentNode == selection.focusNode) {
-        endIndex = index + selection.focusOffset;
-        break;
-      }
-      index += treeWalker.currentNode.length;
-    }
-
-    return {
-      startIndex: Math.min(startIndex, endIndex),
-      endIndex: Math.max(startIndex, endIndex)
-    }
-  }
-
-  /**
-   * Sets the text selection of the instance's root element to the position
-   * specified by the start and end index values. The indexes correspond to
-   * the text content of the element, regardless of what its actual HTML
-   * content is.
-   * @param {Number} startIndex The starting point of the selection.
-   * @param {Number} endIndex The ending point of the selection. If omitted,
-   *     the startIndex is used.
-   */
-  set(startIndex, endIndex = startIndex) {
-
-    let index = 0;
-    let length = this.root.textContent.length;
-    let range = document.createRange();
-    let selection = window.getSelection();
-    let treeWalker = createTextNodeTreeWalker(this.root);
-
-    // Handle negative or out-of-range start/end indexes.
-    if (startIndex < 0) startIndex = length + (startIndex + 1);
-    if (endIndex < 0) endIndex = length + (endIndex + 1);
-    if (startIndex > length) startIndex = length;
-    if (endIndex > length) endIndex = length;
-
-    while (treeWalker.nextNode()) {
-      if (startIndex >= index &&
-          startIndex <= index + treeWalker.currentNode.length) {
-        range.setStart(treeWalker.currentNode, startIndex - index);
-      }
-      if (endIndex >= index &&
-          endIndex <= index + treeWalker.currentNode.length) {
-        range.setEnd(treeWalker.currentNode, endIndex - index);
-        break;
-      }
-      index += treeWalker.currentNode.length;
-    }
-
-    selection.removeAllRanges();
-    selection.addRange(range);
+  return {
+    startIndex: Math.min(startIndex, endIndex),
+    endIndex: Math.max(startIndex, endIndex)
   }
 }
+
+
+/**
+ * Sets the text selection of the instance's root element to the position
+ * specified by the start and end index values. The indexes correspond to
+ * the text content of the element, regardless of what its actual HTML
+ * content is.
+ * @param {Number} startIndex The starting point of the selection.
+ * @param {Number} endIndex The ending point of the selection. If omitted,
+ *     the startIndex is used.
+ */
+Selectable.prototype.set = function(startIndex, endIndex) {
+
+  // `endIndex` is optional.
+  if (!endIndex) endIndex = startIndex;
+
+  var index = 0;
+  var length = this.root.textContent.length;
+  var range = document.createRange();
+  var selection = window.getSelection();
+  var treeWalker = createTextNodeTreeWalker(this.root);
+
+  // Handle negative or out-of-range start/end indexes.
+  if (startIndex < 0) startIndex = length + (startIndex + 1);
+  if (endIndex < 0) endIndex = length + (endIndex + 1);
+  if (startIndex > length) startIndex = length;
+  if (endIndex > length) endIndex = length;
+
+  while (treeWalker.nextNode()) {
+    if (startIndex >= index &&
+        startIndex <= index + treeWalker.currentNode.length) {
+      range.setStart(treeWalker.currentNode, startIndex - index);
+    }
+    if (endIndex >= index &&
+        endIndex <= index + treeWalker.currentNode.length) {
+      range.setEnd(treeWalker.currentNode, endIndex - index);
+      break;
+    }
+    index += treeWalker.currentNode.length;
+  }
+
+  selection.removeAllRanges();
+  selection.addRange(range);
+};
+
 
 
 /**
@@ -83,3 +89,6 @@ export default class Selectable {
 function createTextNodeTreeWalker(element) {
   return document.createTreeWalker(element, NodeFilter.SHOW_TEXT, null, false);
 }
+
+
+module.exports = Selectable;
