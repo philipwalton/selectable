666216c73506f3590cfa1e8501f5ac8ebe843450
Philip Walton <philip@philipwalton.com>
2015-05-07 17:28:36 -0700

    Add error handling to prevent the watch task from exiting.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 var browserify = require('browserify');
 var buffer = require('vinyl-buffer');
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var uglify = require('gulp-uglify');
 
 
+function streamError(err) {
+  gutil.beep();
+  gutil.log(err);
+}
+
+
 gulp.task('javascript', function() {
-  return browserify('./index.js', {standalone: 'Selectable'})
+  browserify('.', {standalone: 'Selectable'})
       .bundle()
+      .on('error', streamError)
       .pipe(source('index.js'))
       .pipe(buffer())
       .pipe(uglify())
